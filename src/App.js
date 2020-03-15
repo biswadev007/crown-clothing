36acff4f7d6b441908e3551cd6c507bd7afdf8f3
biswadev007 <biswa6328@gmail.com>
2020-03-15 10:48:11 +0530

    header component complete 6. Master Project 4. Header Component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from './pages/HomePage/HomePage.component';
 import ShopPage from "./pages/ShopPage/ShopPage.component";
+import Header from "./components/Header/Header.component";
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
